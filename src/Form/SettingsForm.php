6fb85fa28905b7f077661e648633dbd97361b468
Allen Shaw <allen@JoineryHQ.com>
2021-09-01 13:21:04 -0500

    Warn in various cases if configured Twitter user has no twitter accounts.

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -31,12 +31,14 @@ class SettingsForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('tweet_node.settings');
+    $twitterAccountHolderUid = $config->get('twitter_account_holder');
+
     $form['twitter_account_holder'] = [
       '#type' => 'entity_autocomplete',
       '#target_type' => 'user',
       '#title' => $this->t('Site-wided twitter account holder'),
       '#description' => $this->t('Select a user who is configured to tweet on the site-wide Twitter account.'),
-      '#default_value' => \Drupal\user\Entity\User::load($config->get('twitter_account_holder')),
+      '#default_value' => $twitterAccountHolderUser,
     ];
     $form['default_tweet_body'] = [
       '#type' => 'textarea',
@@ -44,6 +46,26 @@ class SettingsForm extends ConfigFormBase {
       '#description' => $this->t('Default value of the &quot;Tweet body&quot; field.'),
       '#default_value' => $config->get('default_tweet_body'),
     ];
+
+    // Only upon initial load of the form (not on submit), warn if configured twitter user
+    // does not actually have a twitter account configured.
+    if (empty($form_state->getUserInput()) && !$form_state->isRebuilding() && $twitterAccountHolderUid) {
+      $twitterAccountHolderUser = \Drupal\user\Entity\User::load($twitterAccountHolderUid);
+      // Get all the Twitter accounts associated with the user account.
+      $user_manager = \Drupal::service('social_post.user_manager');
+      $accounts = $user_manager->getAccounts('social_post_twitter', $twitterAccountHolderUid);
+
+      // If no twitter account linked, warn.
+      if (empty($accounts)) {
+        \Drupal::messenger()->addWarning(t('No Twitter account is configured for the specified Twitter Account Holder (%username). Please <a href="/user/@uid/edit">edit that user</a> to configure Twitter account access.',
+          [
+            '%username' => $twitterAccountHolderUser->getAccountName(),
+            '@uid' => $twitterAccountHolderUid
+          ]
+        ));
+      }
+    }
+
     return parent::buildForm($form, $form_state);
   }
 
